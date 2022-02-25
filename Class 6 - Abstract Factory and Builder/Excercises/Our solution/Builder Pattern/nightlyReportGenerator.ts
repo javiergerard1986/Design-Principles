d6613c1c7250e736b154d74a21d7b4dcae37a4a7
Javier Gerard <javiergerard1986@gmail.com>
2022-02-25 15:40:06 -0300

    Fixing identation

--- Class 6 - Abstract Factory and Builder/Excercises/Our solution/Builder Pattern/nightlyReportGenerator.ts
+++ Class 6 - Abstract Factory and Builder/Excercises/Our solution/Builder Pattern/nightlyReportGenerator.ts
@@ -4,28 +4,28 @@ import { NightlyReportTicket } from "./Tickets/nightlyReportTicket";
 
 export class NightlyReportGenerator {
     
-    director: Director = new Director();
-    ticketBuilder: TicketBuilder = new TicketBuilder();
+  director: Director = new Director();
+  ticketBuilder: TicketBuilder = new TicketBuilder();
 
-    public createNightlyReport(): void {
-        // Do awesome stuff to generate the nightly report information
-        // The following collection will be initializated using actual nightly report information
-        const testFailureResults = ['tc-2 failed! :(', 'tc-3 is unstable! :|']
-        const jiraObjects: NightlyReportTicket[] = [];
+  public createNightlyReport(): void {
+    // Do awesome stuff to generate the nightly report information
+    // The following collection will be initializated using actual nightly report information
+    const testFailureResults = ['tc-2 failed! :(', 'tc-3 is unstable! :|']
+    const jiraObjects: NightlyReportTicket[] = [];
     
-        testFailureResults.forEach((testResult: string) => {
-        this.director.makeNightlyReportTicket(this.ticketBuilder, `${testResult} - INCONSISTENCY`);
+    testFailureResults.forEach((testResult: string) => {
+    this.director.makeNightlyReportTicket(this.ticketBuilder, `${testResult} - INCONSISTENCY`);
 
-          // Process failure results and create tickets
-          jiraObjects.push(this.ticketBuilder.getResult());
-        });
+    // Process failure results and create tickets
+    jiraObjects.push(this.ticketBuilder.getResult());
+    });
     
-        // Now... create the JIRA objects!
-        this.createJIRAInconsistencies(jiraObjects);
-      }
+    // Now... create the JIRA objects!
+      this.createJIRAInconsistencies(jiraObjects);
+  }
     
-      private createJIRAInconsistencies(jiraObjects: NightlyReportTicket[]): void {
-        // Calls JIRA API to create the required incosistencies
-      }        
+  private createJIRAInconsistencies(jiraObjects: NightlyReportTicket[]): void {
+    // Calls JIRA API to create the required incosistencies
+  }        
 
 }
